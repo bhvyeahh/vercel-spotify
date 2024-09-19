7ca6efccfa3108a01b53d508581ea603ec69f9a3
Bhavya Rathore <bhavyarathore575@gmail.com>
2024-09-19 22:10:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // console.log("Lets Write JS");
 
 let currentSong = new Audio();
+
+function formatTime(seconds) {
+  // Ensure seconds is an integer
+  seconds = Math.floor(seconds);
+
+  // Calculate minutes and seconds
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  // Pad with zeros if needed
+  const paddedMinutes = String(minutes).padStart(2, '0');
+  const paddedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${paddedMinutes}:${paddedSeconds}`;
+}
 async function getsongs() {
-  let a = await fetch("http://127.0.0.1:3000/songs");
+  let a = await fetch("http://127.0.0.1:3000/Spotify/songs");
   let response = await a.text();
   // console.log(response) 
   let div = document.createElement("div");
@@ -19,24 +34,29 @@ async function getsongs() {
   return songs;
 }
 
-const playMusic = (track)=>{
+const playMusic = (track, pause = false)=>{
   currentSong.src = "/songs/" + track
-  currentSong.play()
-  play.src = "pause.svg"
+  if(!pause){
+    currentSong.play()
+    play.src = "pause.svg"
+  }
+    document.querySelector(".trackname").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00/00:00"
   audio.play()
 }
 
 async function main() {
-  let songs = await getsongs();
-  // console.log(songs);
 
+  let songs = await getsongs();
+  // playMusic(songs[0], true);
+  // console.log(songs);
    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
 
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
                 <div class="info">
-                  <div class="songName">
-                    ${song.replaceAll("%20", " ")} 
+                <div class="songName">
+                ${song.replaceAll("%20", " ")}
                   </div>
                   <div class="songArtist">
                     Bhavya
@@ -57,17 +77,31 @@ async function main() {
     // Attaching an event listener to play, next, previous
     play.addEventListener("click",()=>{
       if(currentSong.paused){
-        currentSong.play();{
+        currentSong.play()
           play.src = "pause.svg"
-        }
+        
       }
       else{
-        currentSong.pause();{
-          play.src = "play.svg"
-        }
+        currentSong.pause()
+          play.src = "play.svg"  
       }
     })
-    document.querySelector(".trackname").innerHTML = track
-    document.querySelector(".songtime").innerHTML = "00:00/00:00"
+    currentSong.addEventListener("timeupdate",()=>{
+      console.log(currentSong.currentTime, currentSong.duration);
+      document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)}/${formatTime(currentSong.duration)}`
+      document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) *100
++ "%"      
+    })
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+      let percent = (e.offsetX/e.target.getBoundingClientRect().width)* 100  ;
+      document.querySelector(".circle").style.left = percent + "%";  
+      currentSong.currentTime = (currentSong.duration)* percent / 100;
+    })
+    document.querySelector(".hamburger").addEventListener("click", ()=>{
+      document.querySelector(".left").style.left = "0"
+    })
+    document.querySelector(".close").addEventListener("click",()=>{
+      document.querySelector(".left").style.left = "-100%"
+    })
   }
 main();
